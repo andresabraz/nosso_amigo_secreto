c28ee06bada2fec06add7a001c17966f2f32dc82
Andresa Braz <andresabraz@gmail.com>
2018-06-28 17:33:59 -0300

    Setting routes for campaings and members

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,16 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'members/create'
-
-  get 'members/destroy'
-
-  get 'members/update'
-
-  get 'campaigns/show'
-
-  get 'campaigns/index'
-
-  get 'campaigns/create'
-
-  get 'campaigns/update'
-
-  get 'campaigns/destroy'
-
-  get 'campaigns/raffle'
-
-  get 'pages/home'
-
   devise_for :users, :controllers => { registrations: 'registrations' }
-  #Necessário apenas para ver o painel do sidekiq
+  #Necessário para ver o painel do sidekiq
   mount Sidekiq::Web => '/sidekiq'
+
+  root to: 'pages#home'
+  resources :campaigns, except: [:new] do
+    post 'raffle', on: :member
+    # post 'raffle', on: :collection
+  end
+  
+  get 'members/:token/opened', to: 'members#opened'
+  resources :members, only: [:create, :destroy, :update]
 end
